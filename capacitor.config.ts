e1ec14a7e442320066429bf4e481ffa40fcf3573
Jonathan Katz <tiko2015@gmail.com>
2024-10-30 11:36:11 -0300

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.essapp.app',
+  appName: 'essapp',
+  webDir: 'dist/essapp/browser'
+};
+
+export default config;
