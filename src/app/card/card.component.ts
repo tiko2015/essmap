f4b91093c06c2c659fefe4aefb844a69acc981d1
Jonathan Katz <tiko2015@gmail.com>
2024-11-05 16:07:08 -0300

    cambiis en card action

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -25,34 +25,4 @@ export class CardComponent {
     }
     this.centerMap.emit(this.entidad);
   }
-
-  link() {
-    if (this.entidad?.tipo === 'chasqui') {
-      return 'https://chasqui.qa.1961.com.ar/' + this.entidad?.nid;
-    }
-    return 'https://essapp.coop/node/' + this.entidad?.nid;
-  }
-  share() {
-    const link = {
-      title: this.entidad?.nombre,
-      url: `${window.location.origin}/#${this.entidad?.nid}`, // URL completa para que funcione tanto en web como en Android
-      text: this.entidad?.nombre
-    };
-
-    if (Capacitor.getPlatform() === 'web') {
-      // Usar la Web Share API si está disponible en la web
-      if (navigator.share) {
-        navigator.share(link).catch(error => console.error('Error compartiendo en web:', error));
-      } else {
-        console.warn('Web Share API no está disponible en este navegador.');
-      }
-    } else {
-      // Usar Capacitor Share en dispositivos móviles
-      Share.share({
-        title: link.title,
-        text: link.text,
-        url: link.url,
-      }).catch(error => console.error('Error compartiendo en dispositivo:', error));
-    }
-  }
 }
