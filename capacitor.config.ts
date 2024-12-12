653258c622e97c3f878deef2a5b65c860e929acb
Jonathan Katz <tiko2015@gmail.com>
2024-12-12 09:03:55 -0300

    fix: cambio de iconos para apk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.essapp.app',
-  appName: 'essapp',
+  appName: 'ESSApp',
   webDir: 'dist/essapp/browser',
   plugins: {
     SplashScreen: {
