c01db9293a64149d3deda084424a917b80e3fa5e
Jonathan Katz <tiko2015@gmail.com>
2026-08-14 09:52:37 -0300

    cambio de api url

--- frontend/src/app/graphql.config.ts
+++ frontend/src/app/graphql.config.ts
@@ -1,2 +1,2 @@
-export const myUrl = 'http://localhost:3000/shop-api';
-//export const myUrl = 'https://huemul-backend.nube.chasqui.site/shop-api';
+//export const myUrl = 'http://localhost:3000/shop-api';
+export const myUrl = 'https://huemul-backend.nube.proyectochasqui.com/shop-api';
